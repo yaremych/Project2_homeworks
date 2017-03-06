03f286c7a4f1993d8bbba83c0d6df56a99fd4f7f
Haley Yaremych <yaremych@umich.edu>
2017-03-05 22:37:45 -0500

    PROJECT 2: Adds Completed Part Three and cached data file

--- SI206W17_project2.py
+++ SI206W17_project2.py
@@ -191,10 +191,18 @@ def get_five_tweets(string):
 
 ## PART 3 (b) - Write one line of code to invoke the get_five_tweets function with the phrase "University of Michigan" and save the result in a variable five_tweets.
 
-
+five_tweets = get_five_tweets("University of Michigan")
 
 
 ## PART 3 (c) - Iterate over the five_tweets list, invoke the find_urls function that you defined in Part 1 on each element of the list, and accumulate a new list of each of the total URLs in all five of those tweets in a variable called tweet_urls_found. 
+
+tweet_urls_found = []
+
+for tweet in five_tweets:
+	url_results = find_urls(tweet)
+
+	for item in url_results:
+		tweet_urls_found.append(item)
 
 
 
